bf89f8665ffc7d1e9c7a801c90f91a7bf9907799
Dario Bugmann <dario.bugmann@outlook.com>
2023-11-13 07:50:39 +0100

    use lazy.nvim properly

---
+++ nvim/lua/core/plugins/lsp/mason.lua
@@ -0,0 +1,24 @@
+-- Find all lsps supported by mason
+-- https://github.com/williamboman/mason-lspconfig.nvim
+
+return {
+  'williamboman/mason.nvim',
+  {
+    dependencies = {
+      'williamboman/mason-lspconfig.nvim',
+      "jay-babu/mason-nvim-dap.nvim",
+    },
+    config = function()
+      require('mason').setup()
+
+      require('mason-lspconfig').setup({
+        ensure_installed = { 'lua_ls', 'tsserver', 'csharp_ls' }
+      })
+
+      require('mason-nvim-dap').setup({
+        automatic_installation = true,
+        ensure_installed = { 'js-debug-adapter', 'netcoredbg' }
+      })
+    end
+  }
+}
