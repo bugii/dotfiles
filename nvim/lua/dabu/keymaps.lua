f2358a34ffcd76892e568e729d2a14db2af5b16f
Dario Bugmann <dario.bugmann@outlook.com>
2023-11-14 16:47:33 +0100

    use absolute line numbers

--- nvim/lua/dabu/keymaps.lua
+++ nvim/lua/dabu/keymaps.lua
@@ -7,7 +7,7 @@ vim.opt.laststatus = 2
 vim.opt.autowrite = true
 vim.opt.cursorline = true
 vim.opt.autoread = true
-vim.opt.relativenumber = true
+vim.opt.number = true
 
 -- use spaces for tabs and whatnot
 vim.opt.tabstop = 2
