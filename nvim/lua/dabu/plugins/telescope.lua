90154b85d9c6a0f24661e57a593526a988bce1d9
Dario Bugmann <dario.bugmann@outlook.com>
2023-11-16 17:11:57 +0100

    add git-worktrees plugin

--- nvim/lua/dabu/plugins/telescope.lua
+++ nvim/lua/dabu/plugins/telescope.lua
@@ -1,19 +1,36 @@
 return {
 	"nvim-telescope/telescope.nvim",
 	tag = "0.1.4",
-	dependencies = { "nvim-lua/plenary.nvim" },
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"ThePrimeagen/git-worktree.nvim",
+	},
 	config = function()
 		local builtin = require("telescope.builtin")
+		local themes = require("telescope.themes")
+		require("telescope").load_extension("git_worktree")
 
 		vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "Find in Files" })
 		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "[F]ind by [G]rep" })
 		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "[F]ind [H]elp" })
 		vim.keymap.set("n", "<leader>ff", function()
-			builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+			builtin.current_buffer_fuzzy_find(themes.get_dropdown({
 				winblend = 10,
 				previewer = false,
 			}))
 		end, { desc = "[ff] Fuzzily search in current buffer" })
 		vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "[F]ind [D]iagnostics" })
+		vim.keymap.set(
+			"n",
+			"<leader>gw",
+			require("telescope").extensions.git_worktree.git_worktrees,
+			{ desc = "Show [G]it [W]orktrees" }
+		)
+		vim.keymap.set(
+			"n",
+			"<leader>cgw",
+			require("telescope").extensions.git_worktree.create_git_worktree,
+			{ desc = "[C]reate [G]it [W]orktrees" }
+		)
 	end,
 }
