5415b2b1bcffaeed80d2b5fabcda6304233dc18d
Dario Bugmann <dario.bugmann@outlook.com>
2023-11-17 09:42:29 +0100

    make snipptes usable

--- nvim/lua/dabu/plugins/autocomplete.lua
+++ nvim/lua/dabu/plugins/autocomplete.lua
@@ -1,3 +1,9 @@
+local has_words_before = function()
+	unpack = unpack or table.unpack
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 return {
 	"hrsh7th/nvim-cmp",
 	dependencies = {
@@ -7,19 +13,32 @@ return {
 	},
 	config = function()
 		local cmp = require("cmp")
+		local luasnip = require("luasnip")
 		require("luasnip.loaders.from_vscode").lazy_load()
 
 		cmp.setup({
 			mapping = cmp.mapping.preset.insert({
-				["<C-b>"] = cmp.mapping.scroll_docs(-4),
 				["<C-f>"] = cmp.mapping.scroll_docs(4),
+				["<C-b>"] = cmp.mapping.scroll_docs(-4),
 				["<C-o>"] = cmp.mapping.complete(),
 				["<C-e>"] = cmp.mapping.abort(),
 				["<CR>"] = cmp.mapping.confirm({ select = true }),
+				["<Tab>"] = cmp.mapping(function()
+					if luasnip.jumpable(1) then
+						luasnip.jump(1)
+					end
+				end, { "i", "s" }),
+
+				["<S-Tab>"] = cmp.mapping(function()
+					if luasnip.jumpable(-1) then
+						luasnip.jump(-1)
+					end
+				end, { "i", "s" }),
 			}),
+
 			snippet = {
 				expand = function(args)
-					require("luasnip").lsp_expand(args.body)
+					luasnip.lsp_expand(args.body)
 				end,
 			},
 			sources = cmp.config.sources({
