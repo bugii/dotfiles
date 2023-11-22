a0fbb50a93e3f78db525e7bf710ff074cb20f0de
Dario Bugmann <dario.bugmann@outlook.com>
2023-11-22 21:26:38 +0100

    add commenting support for tsx/jsx

--- nvim/lua/dabu/plugins/comment.lua
+++ nvim/lua/dabu/plugins/comment.lua
@@ -1,4 +1,12 @@
 return {
 	"numToStr/Comment.nvim",
-	opts = {},
+	dependencies = {
+		"JoosepAlviste/nvim-ts-context-commentstring",
+	},
+	config = function()
+		require("Comment").setup({
+			-- this is needed for support in jsx/tsx
+			pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+		})
+	end,
 }
