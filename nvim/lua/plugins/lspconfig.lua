3a2fc941853a30ceb5c9a35e672c071def72cf16
Dario Bugmann <dario.bugmann@outlook.com>
2023-11-27 21:09:34 +0100

    more intuitive diagnostics keymaps

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -16,8 +16,8 @@ return {
 		-- Global mappings.
 		-- See `:help vim.diagnostic.*` for documentation on any of the below functions
 		vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Diagnostic: open float" })
-		vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Diagnostic: goto prev" })
-		vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Diagnostic: goto next" })
+		vim.keymap.set("n", "<leader>dp", vim.diagnostic.goto_prev, { desc = "[D]iagnostic: goto [p]rev" })
+		vim.keymap.set("n", "<leader>dn", vim.diagnostic.goto_next, { desc = "[D]iagnostic: goto [n]ext" })
 		vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Diagnostic: open diagnostics list" })
 
 		-- Use LspAttach autocommand to only map the following keys
