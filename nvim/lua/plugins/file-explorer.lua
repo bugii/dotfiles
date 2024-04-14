9d5899a26951705d45fb726526cb2e7b1f54cde3
dario <dario.bugmann@outlook.com>
2024-04-14 17:03:26 +0200

    update deps

--- nvim/lua/plugins/file-explorer.lua
+++ nvim/lua/plugins/file-explorer.lua
@@ -1,13 +1,16 @@
 return {
-	"nvim-tree/nvim-tree.lua",
-	dependencies = {
-		"nvim-tree/nvim-web-devicons",
-	},
-	config = function()
-		require("nvim-tree").setup({
-			view = { adaptive_size = true, side = "right" },
-		})
-
-		vim.keymap.set("n", "<c-n>", ":NvimTreeFindFileToggle<CR>")
-	end,
+  -- "nvim-tree/nvim-tree.lua",
+  -- dependencies = {
+  -- 	"nvim-tree/nvim-web-devicons",
+  -- },
+  -- config = function()
+  -- 	require("nvim-tree").setup({
+  -- 		view = { adaptive_size = true, side = "right" },
+  -- 	})
+  --
+  -- 	vim.keymap.set("n", "<c-n>", ":NvimTreeFindFileToggle<CR>")
+  -- end,
+  'stevearc/oil.nvim',
+  opts = {},
+  dependencies = { 'nvim-tree/nvim-web-devicons' }
 }
