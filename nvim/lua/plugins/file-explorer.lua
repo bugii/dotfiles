99a02a0ace463f20161467397b219cbee96684cc
dario <dario.bugmann@outlook.com>
2024-04-14 17:16:39 +0200

    adding oil.nvim

--- nvim/lua/plugins/file-explorer.lua
+++ nvim/lua/plugins/file-explorer.lua
@@ -1,16 +1,13 @@
 return {
-  -- "nvim-tree/nvim-tree.lua",
-  -- dependencies = {
-  -- 	"nvim-tree/nvim-web-devicons",
-  -- },
-  -- config = function()
-  -- 	require("nvim-tree").setup({
-  -- 		view = { adaptive_size = true, side = "right" },
-  -- 	})
-  --
-  -- 	vim.keymap.set("n", "<c-n>", ":NvimTreeFindFileToggle<CR>")
-  -- end,
-  'stevearc/oil.nvim',
-  opts = {},
-  dependencies = { 'nvim-tree/nvim-web-devicons' }
+  "nvim-tree/nvim-tree.lua",
+  dependencies = {
+  	"nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+  	require("nvim-tree").setup({
+  		view = { adaptive_size = true, side = "right" },
+  	})
+
+  	vim.keymap.set("n", "<c-n>", ":NvimTreeFindFileToggle<CR>")
+  end,
 }
