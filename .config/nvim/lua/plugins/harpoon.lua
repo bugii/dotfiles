b1c8796d33605b9e3ba21167827a168553947de0
Dario Bugmann <dario.bugmann@outlook.com>
2024-10-16 14:00:48 +0200

    update

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -53,19 +53,6 @@ return {
 			toggle_telescope(harpoon:list())
 		end, { desc = "Open harpoon window" })
 
-		vim.keymap.set("n", "<C-h>", function()
-			harpoon:list():select(1)
-		end)
-		vim.keymap.set("n", "<C-t>", function()
-			harpoon:list():select(2)
-		end)
-		vim.keymap.set("n", "<C-n>", function()
-			harpoon:list():select(3)
-		end)
-		vim.keymap.set("n", "<C-s>", function()
-			harpoon:list():select(4)
-		end)
-
 		-- Toggle previous & next buffers stored within Harpoon list
 		vim.keymap.set("n", "<S-H>", function()
 			harpoon:list():prev()
