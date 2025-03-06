73173d0d23cad82735cb2ab9c7d2fbbfed9b4c30
Dario Bugmann <dario.bugmann@outlook.com>
2025-03-06 13:01:19 +0100

    improve command completions and in dadbod via vim-dadbod-completion

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -1,64 +1,78 @@
 return {
-	"saghen/blink.cmp",
-	lazy = false, -- lazy loading handled internally
-	-- optional: provides snippets for the snippet source
-	dependencies = {
-		"rafamadriz/friendly-snippets",
-	},
-	-- use a release tag to download pre-built binaries
-	version = "v0.*",
+  "saghen/blink.cmp",
+  lazy = false, -- lazy loading handled internally
+  -- optional: provides snippets for the snippet source
+  dependencies = {
+    "rafamadriz/friendly-snippets",
+  },
+  -- use a release tag to download pre-built binaries
+  version = "v0.*",
 
-	opts = {
-		keymap = {
-			preset = "default",
-			["<C-l>"] = { "show", "show_documentation", "hide_documentation" },
-			["<C-k>"] = { "snippet_forward", "fallback" },
-			["<C-j>"] = { "snippet_backward", "fallback" },
-		},
-		appearance = {
-			-- Sets the fallback highlight groups to nvim-cmp's highlight groups
-			-- Useful for when your theme doesn't support blink.cmp
-			-- will be removed in a future release
-			use_nvim_cmp_as_default = true,
-			-- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-			-- Adjusts spacing to ensure icons are aligned
-			nerd_font_variant = "mono",
-		},
+  opts = {
+    keymap = {
+      preset = "default",
+      ["<C-l>"] = { "show", "show_documentation", "hide_documentation" },
+      ["<C-k>"] = { "snippet_forward", "fallback" },
+      ["<C-j>"] = { "snippet_backward", "fallback" },
+    },
+    appearance = {
+      -- Sets the fallback highlight groups to nvim-cmp's highlight groups
+      -- Useful for when your theme doesn't support blink.cmp
+      -- will be removed in a future release
+      use_nvim_cmp_as_default = true,
+      -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+      -- Adjusts spacing to ensure icons are aligned
+      nerd_font_variant = "mono",
+    },
 
-		-- default list of enabled providers defined so that you can extend it
-		-- elsewhere in your config, without redefining it, via `opts_extend`
-		sources = {
-			default = { "lsp", "path", "snippets", "buffer" },
-		},
+    -- default list of enabled providers defined so that you can extend it
+    -- elsewhere in your config, without redefining it, via `opts_extend`
+    sources = {
+      default = { "lsp", "path", "snippets", "buffer" },
+      per_filetype = {
+        sql = { "snippets", "dadbod", "buffer" },
+      },
+      -- add vim-dadbod-completion to your completion providers
+      providers = {
+        dadbod = { name = "Dadbod", module = "vim_dadbod_completion.blink" },
+      },
+    },
 
-		-- experimental signature help support
-		signature = { enabled = true },
+    -- experimental signature help support
+    signature = { enabled = true },
 
-		completion = {
-			documentation = { auto_show = true, auto_show_delay_ms = 500 },
-			menu = {
-				draw = {
-					components = {
-						kind_icon = {
-							ellipsis = false,
-							text = function(ctx)
-								local kind_icon, _, _ = require("mini.icons").get("lsp", ctx.kind)
-								return kind_icon
-							end,
+    completion = {
+      documentation = { auto_show = true, auto_show_delay_ms = 500 },
+      menu = {
+        draw = {
+          components = {
+            kind_icon = {
+              ellipsis = false,
+              text = function(ctx)
+                local kind_icon, _, _ = require("mini.icons").get("lsp", ctx.kind)
+                return kind_icon
+              end,
 
-							-- Optionally, you may also use the highlights from mini.icons
-							highlight = function(ctx)
-								local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
-								return hl
-							end,
-						},
-					},
-				},
-			},
-		},
-	},
+              -- Optionally, you may also use the highlights from mini.icons
+              highlight = function(ctx)
+                local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
+                return hl
+              end,
+            },
+          },
+        },
+      },
+    },
+    cmdline = {
+      keymap = {
+        ["<C-l>"] = { "show", "hide" },
+        ["<C-y>"] = { "accept" },
+      },
+      completion = { menu = { auto_show = true } },
+    },
+  },
 
-	-- allows extending the providers array elsewhere in your config
-	-- without having to redefine it
-	opts_extend = { "sources.default" },
+  -- allows extending the providers array elsewhere in your config
+  -- without having to redefine it
+  opts_extend = { "sources.default" },
 }
