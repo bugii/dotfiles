50cfc1914e22947101a7905104f3336906313664
dario <dario.bugmann@outlook.com>
2025-03-10 07:49:21 +0100

    add package-info

---
+++ .config/nvim/lua/plugins/package-info.lua
@@ -0,0 +1,12 @@
+return {
+  "vuki656/package-info.nvim",
+  config = function()
+    require("package-info").setup()
+
+    vim.keymap.set({ "n" }, "<leader>nt", require("package-info").toggle, { silent = true, noremap = true })
+    vim.keymap.set({ "n" }, "<leader>nu", require("package-info").update, { silent = true, noremap = true })
+    vim.keymap.set({ "n" }, "<leader>nd", require("package-info").delete, { silent = true, noremap = true })
+    vim.keymap.set({ "n" }, "<leader>ni", require("package-info").install, { silent = true, noremap = true })
+    vim.keymap.set({ "n" }, "<leader>np", require("package-info").change_version, { silent = true, noremap = true })
+  end,
+}
