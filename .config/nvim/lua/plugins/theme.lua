d0988bbc2b1ee7fb55aa3da1024dad9b0dc0fd44
Dario Bugmann <dario.bugmann@outlook.com>
2025-04-11 09:14:00 +0200

    some new coloring stuff

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -2,154 +2,107 @@ return {
   {
     "folke/tokyonight.nvim",
     lazy = true,
-    -- opts = {},
+    opts = {
+      transparent = true,
+    },
   },
   {
     "catppuccin/nvim",
     name = "catppuccin",
     lazy = true,
     opts = {
-      flavour = "macchiato", -- latte, frappe, macchiato, mocha
-      transparent_background = true, -- disables setting the background color.
-      integrations = {
-        cmp = true,
-        gitsigns = true,
-        treesitter = true,
-        noice = true,
-        neotest = true,
-        mason = true,
-        -- which_key = true,
-        -- leap = true,
-        dap = true,
-        dap_ui = true,
-        -- notify = true,
-        -- telescope = {
-        -- 	enabled = true,
-        -- },
-        native_lsp = {
-          enabled = true,
-        },
-        -- indent_blankline = {
-        -- 	enabled = true,
-        -- },
-        -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
-      },
-      -- color_overrides = {
-      -- 	mocha = {
-      -- 		base = "#000000",
-      -- 		mantle = "#000000",
-      -- 		crust = "#000000",
-      -- 	},
-      -- },
-      -- highlight_overrides = {
-      -- 	mocha = function(C)
-      -- 		return {
-      -- 			TabLineSel = { bg = C.pink },
-      -- 			CmpBorder = { fg = C.surface2 },
-      -- 			Pmenu = { bg = C.none },
-      -- 		}
-      -- 	end,
-      -- },
+      flavour = "mocha",
+      transparent_background = true,
     },
   },
   {
     "rebelot/kanagawa.nvim",
     lazy = true,
-    -- opts = {},
-  },
-  {
-    "thesimonho/kanagawa-paper.nvim",
-    lazy = true,
-    -- opts = {},
+    opts = {
+      transparent = true,
+    },
   },
   {
     "rose-pine/neovim",
     lazy = true,
     name = "rose-pine",
+    opts = {
+      styles = {
+        bold = true,
+        italic = true,
+        transparency = true,
+      },
+    },
   },
-  { "EdenEast/nightfox.nvim", lazy = true },
+  {
+    "EdenEast/nightfox.nvim",
+    opts = {
+      options = {
+        transparent = true,
+        styles = {
+          comments = "italic",
+        },
+      },
+    },
+    lazy = true,
+  },
   {
     "sample-usr/rakis.nvim",
     lazy = true,
-    -- opts = {},
-    config = function()
-      require("rakis").setup({
-        theme = {
-          variant = "default",
-          extensions = { mini = true },
-          overrides = function(colors)
-            return {
-              MiniStatuslineModeNormal = { fg = colors.bg_solid, bg = colors.orange02, bold = true },
-            }
-          end,
-        },
-      })
-    end,
-  },
-  {
-    "embark-theme/vim",
-    lazy = true,
-    -- opts = {},
+    opts = {
+      transparent = true,
+      italic_comments = true,
+    },
   },
   {
     "Everblush/nvim",
     name = "everblush",
-    -- priority = 1000,
-    -- config = function() vim.cmd("colorscheme everblush") end,
-  },
-  {
-    "neanias/everforest-nvim",
-    version = false,
     lazy = true,
+    opts = {
+      transparent_background = true,
+    },
   },
   {
     "uloco/bluloco.nvim",
     lazy = true,
-    -- priority = 1000,
     dependencies = { "rktjmp/lush.nvim" },
-    -- config = function() vim.cmd("colorscheme bluloco-dark") end,
-  },
-  {
-    "yorumicolors/yorumi.nvim",
-    -- lazy = true,
-    priority = 1000,
-    opts = {},
-    config = function() vim.cmd("colorscheme yorumi") end,
+    opts = {
+      transparent = true,
+      italics = true,
+    },
   },
   {
     "dasupradyumna/midnight.nvim",
     lazy = true,
-    -- priority = 1000,
-    -- opts = {},
-    -- config = function() vim.cmd("colorscheme midnight") end,
-  },
-  {
-    "nyoom-engineering/oxocarbon.nvim",
-    lazy = true,
-    -- opts = {},
+    opts = {},
   },
   {
     "wtfox/jellybeans.nvim",
-    -- priority = 1000,
-    lazy = true,
-    -- opts = {},
-    -- config = function() vim.cmd([[colorscheme jellybeans]]) end,
-  },
-  {
-    "tiagovla/tokyodark.nvim",
-    lazy = true,
-    -- priority = 1000,
-    -- opts = {},
-    -- config = function() vim.cmd("colorscheme tokyodark") end,
+    priority = 1000,
+    opts = {
+      transparent = false,
+      italics = true,
+      flat_ui = false,
+      on_colors = function(c)
+        local light_bg = "#ffffff"
+        local dark_bg = "#000000"
+        c.background = vim.o.background == "light" and light_bg or dark_bg
+      end,
+      on_highlights = function(hl, c) hl.NormalFloat = { bg = c.background } end,
+    },
+    config = function(_, opts)
+      require("jellybeans").setup(opts)
+      vim.cmd("colorscheme jellybeans")
+    end,
   },
   {
     "scottmckendry/cyberdream.nvim",
     lazy = true,
-    -- opts = {}
+    opts = {},
   },
   {
     "S-Spektrum-M/odyssey.nvim",
     lazy = true,
-    -- opts = {}
+    opts = {},
   },
 }
