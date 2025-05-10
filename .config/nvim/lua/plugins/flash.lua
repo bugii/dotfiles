a439b6a324d269c7647c958bf7961e5ec1f80a1c
Dario Bugmann <dario.bugmann@outlook.com>
2025-05-10 10:57:04 +0200

    fix flash and make lualine nicer

--- .config/nvim/lua/plugins/flash.lua
+++ .config/nvim/lua/plugins/flash.lua
@@ -3,7 +3,18 @@ return {
   event = "VeryLazy",
   opts = {},
   keys = {
-    { "<CR>", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    {
+      "<CR>",
+      mode = { "n", "x", "o" },
+      function()
+        if not vim.tbl_contains({ vim.bo.filetype }, "vim") then
+          require("flash").jump()
+        else
+          vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<CR>", true, false, true), "n", true)
+        end
+      end,
+      desc = "Flash",
+    },
     {
       "<leader><CR>",
       mode = { "n", "x", "o" },
