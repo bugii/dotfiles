76e0de7baf11c367afea735661c31ab62098af05
Dario Bugmann <dario.bugmann@zuehlke.com>
2025-11-19 21:33:56 +0100

    add mini visits again 👀

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -10,6 +10,7 @@ return {
     require("mini.diff").setup()
     require("mini.sessions").setup({ autoread = true })
     require("mini.git").setup()
+    local MiniVisits = require("mini.visits")
     local MiniHipatterns = require("mini.hipatterns")
     local MiniFiles = require("mini.files")
     local MiniBufremove = require("mini.bufremove")
@@ -35,6 +36,16 @@ return {
         hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
         todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
         note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
+      },
+    })
+
+    MiniVisits.setup({
+      list = {
+        filter = function(opts)
+          if vim.startswith(opts.path, "fugitive:") then return false end
+          if vim.fn.fnamemodify(opts.path, ":t") == "COMMIT_EDITMSG" then return false end
+          return true
+        end,
       },
     })
 
@@ -65,5 +76,27 @@ return {
         end,
       },
     })
+
+    local sort_recent = MiniVisits.gen_sort.default({ recency_weight = 1 })
+    vim.keymap.set(
+      "n",
+      "L",
+      function() MiniVisits.iterate_paths("backward", nil, { sort = sort_recent, wrap = true }) end,
+      { desc = "Go to last visited path" }
+    )
+
+    vim.keymap.set(
+      "n",
+      "H",
+      function() MiniVisits.iterate_paths("forward", nil, { sort = sort_recent, wrap = true }) end,
+      { desc = "Go to next visited path" }
+    )
+
+    vim.keymap.set(
+      "n",
+      "<leader>fv",
+      function() MiniVisits.select_path(nil, { sort = sort_recent }) end,
+      { desc = "Find Visits" }
+    )
   end,
 }
